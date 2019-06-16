a2caea5aa94862192746c4eba5f95519ed6bd640
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-06-16 20:44:04 +0200

    working show tablei item

--- template-parts/7-items/_2-info.php
+++ template-parts/7-items/_2-info.php
@@ -10,40 +10,40 @@
 			</div>
 		</div>
 		<div class="info__full-box">
-			<?php if ( have_rows( 'item_table' ) ):
-				while( have_rows( 'item_table' ) ): the_row();
-			
-				if( get_field( 'item_table_show' ) ): ?>
-					<h2 class="sub-heading sub-heading--black">Wymiary</h2>
-				<?php endif; ?>
+			<?php if( get_field( 'item_table_show' ) ):
+				if ( have_rows( 'item_table' ) ):
+					while( have_rows( 'item_table' ) ): the_row();?>
+				
+				<h2 class="sub-heading sub-heading--black">Wymiary</h2>
 
-			<table class="info__table">
-				<tbody>
+				<table class="info__table">
+					<tbody>
 
-					<?php if (have_rows( 'item_table_row') ):
-						while( have_rows( 'item_table_row') ): the_row();
+						<?php if (have_rows( 'item_table_row') ):
+							while( have_rows( 'item_table_row') ): the_row();
 
-						$text_1 = get_sub_field( 'item_table_text_1' );
-						$text_2 = get_sub_field( 'item_table_text_2' );
+							$text_1 = get_sub_field( 'item_table_text_1' );
+							$text_2 = get_sub_field( 'item_table_text_2' );
 
-						?>
+							?>
 
-						<tr>
-							<td><?php echo $text_1 ?></td>
-							<td><?php echo $text_2 ?></td>
-						</tr>
+							<tr>
+								<td><?php echo $text_1 ?></td>
+								<td><?php echo $text_2 ?></td>
+							</tr>
 
-					<?php endwhile; endif; ?>
+						<?php endwhile; endif; ?>
 
-				</tbody>
-			</table>
+					</tbody>
+				</table>
 
-			<?php if( get_field( 'item_show_possibility_to_resize' ) ) : ?>
-				<span>*Możliwość dopasowania wymiaru wg życzenia klienta</span>
-			<?php endif;
+				<?php if( get_field( 'item_show_possibility_to_resize' ) ) : ?>
+					<span>*Możliwość dopasowania wymiaru wg życzenia klienta</span>
+				<?php endif;
 
-		endwhile; endif; ?>
+			endwhile; endif;
 
+		endif; ?>
 		</div>
 	</div>
 </section>
