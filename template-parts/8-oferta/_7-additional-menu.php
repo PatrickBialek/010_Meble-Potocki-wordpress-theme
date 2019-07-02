4acd31cdab5244b430030cffb8db6776866513d5
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-07-02 17:42:22 +0200

    old libs are delited

--- template-parts/8-oferta/_7-additional-menu.php
+++ template-parts/8-oferta/_7-additional-menu.php
@@ -1,9 +1,9 @@
 <nav class="additional-menu">
 	<div class="additional-menu__content-box">
 		<ul class="additional-menu__nav-list">
-			<li><a href="#tables">Stoły</a></li>
-			<li><a href="#chairs">Krzesła</a></li>
-			<li><a href="#cupboards">Ławy</a></li>
+			<li><a data-scroll href="#tables">Stoły</a></li>
+			<li><a data-scroll href="#chairs">Krzesła</a></li>
+			<li><a data-scroll href="#cupboards">Ławy</a></li>
 		</ul>
 	</div>
 </nav>
