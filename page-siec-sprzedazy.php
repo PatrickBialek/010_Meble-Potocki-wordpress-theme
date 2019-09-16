2e336028fc7be10c42e78980560be41ebb484c99
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-09-16 21:49:07 +0200

    added styles for distribution

--- page-siec-sprzedazy.php
+++ page-siec-sprzedazy.php
@@ -8,6 +8,8 @@ include get_template_directory() . "/template-parts/3-single/_1-heading.php";
 
 include get_template_directory() . "/template-parts/9-siec-sprzedazy/_1-basic-info.php";
 
+include get_template_directory() . "/template-parts/9-siec-sprzedazy/_2-distribution_network.php";
+
 ?>
 
 </main>
--- template-parts/9-siec-sprzedazy/_2-distribution_network.php
+++ template-parts/9-siec-sprzedazy/_2-distribution_network.php
@@ -3,8 +3,8 @@
 		<h1 class="sub-heading sub-heading--black"><?php echo $heading ?></h1>
 		<div class="distribution-network__content-box">
 
-		<?php if( have_rows( 'distribution-network_tiles' ) ): 
-			while( have_rows( 'distribution-network_tiles' ) ): the_row(); ?>
+		<?php if( have_rows( 'distribution_network_tiles' ) ): 
+			while( have_rows( 'distribution_network_tiles' ) ): the_row(); ?>
 
 				<div class="distribution-network__tiles-row">
 
@@ -18,17 +18,30 @@
 								$icon = get_sub_field( 'icon' );
 								$name = get_sub_field( 'name' );
 								$addres_1 = get_sub_field( 'addres_1' );
+								$addres_2 = get_sub_field( 'addres_2' );
 								$open_hours = get_sub_field( 'open_hours' );
 								$common_days = get_sub_field( 'common_days' );
 								$weekends = get_sub_field( 'weekends' );
 								$tel = get_sub_field( 'tel' );
-								$phone = get_sub_field( 'phone' );
+								$mail = get_sub_field( 'mail' );
 
 								?>
 
-								<div class="distribution-network__single-tile">
+								<h3 class="distribution-network__voivodeship"><?php echo $voivodeship ?></h3>
+								<div class="distribution-network__row">
 									<div class="distribution-network__icon-box"><img class="gallery__image" src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>" /></div>
-									<div class="wdistribution-network__text-box"><?php echo $textarea ?></div>
+									<div class="distribution-network__text-box">
+										<span><?php echo $name ?></span>
+										<span><?php echo $addres_1 ?></span>
+										<span><?php echo $addres_2 ?></span>
+									</div>
+								</div>
+								<div class="distribution-network__row">
+									<span><?php echo $open_hours ?></span>
+									<span><?php echo $common_days ?></span>
+									<span><?php echo $weekends ?></span>
+									<span><?php echo $tel ?></span>
+									<span><?php echo $mail ?></span>
 								</div>
 								
 					<?php endwhile; endif; endwhile; endif; ?>
